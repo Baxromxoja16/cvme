5f002d794f8c2755f21b1df3b5e63057452c08a1
baxromxoja16 <aglamof@mail.ru>
2026-01-11 16:49:21 +0500

    fix: yml

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.cvme.uz/api',
-    authUrl: 'https://api.cvme.uz/api/auth',
-    baseUrl: 'https://cvme.uz',
-    baseDomain: 'cvme.uz',
-    fileUrl: 'https://api.cvme.uz'
+    apiUrl: 'https://piggy-bank-bhxy.onrender.com/api',
+    authUrl: 'https://piggy-bank-bhxy.onrender.com/api/auth',
+    baseUrl: 'https://piggy-bank-bhxy.onrender.com',
+    baseDomain: 'piggy-bank-bhxy.onrender.com',
+    fileUrl: 'https://piggy-bank-bhxy.onrender.com'
 };
 
